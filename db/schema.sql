880eca88697c48bc351524e07a8587c756eb8c96
Thomas G Musler <thomas.g.musler@gmail.com>
2021-04-25 15:32:54 -0400

    schema add

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE departments(
+id INT AUTO_INCREMENT PRIMARY KEY,
+department_name VARCHAR(30)
+);
+
+CREATE TABLE roles (
+id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30),
+salary DECIMAL(8,2),
+department_id INT,
+);
+
+CREATE TABLE employees(
+id INT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT,
+);
+
