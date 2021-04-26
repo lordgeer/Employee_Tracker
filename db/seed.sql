cad5251a79b717246229dc0c5927f32855909892
Thomas G Musler <thomas.g.musler@gmail.com>
2021-04-26 16:16:01 -0400

    added seed for population

---
+++ db/seed.sql
@@ -0,0 +1,13 @@
+USE employee_db;
+
+INSERT INTO department (name)
+VALUES 
+('Board'),('Fleet'),('Management'),('"Human Resources"'),('Special Projects'),('Security');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Partner', 1000000, 1),('Fleet Admiral', 600000, 2),('Underboss', 500000, 3),('Bounty Hunter(retainer)', 75000, 4),('Special Advisor', 90000, 5),('Mercenary', 40000, 6);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('Urai', 'Fenn', 1, 458),('Jerid', 'Sykes', 2, 276),('Venlana', 'Sipal', 3, 486),('Bossk', 'NO DATA', 4, 126),('Silri', 'NO DATA', 5, 724),('Taylor', 'Wilson', 6, 157);
